e13e1bdfd82b49d5aac1e5f4f4bee62cf93bb9cf
Yuda Adi Pratama <yudaa0110@gmail.com>
2022-02-27 10:21:29 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+const notes = [];
+
+module.exports = notes;
---
+++ src/routes.js
@@ -0,0 +1,44 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+
+const {
+ addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler,
+} = require('./handler');
+
+/* eslint-disable linebreak-style */
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
